6a452c621dd9795cd212690aa89045b4bdf17478
2henC <nguyenthanh8a2@gmail.com>
2023-09-24 00:10:10 +0700

    them class va onClick

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import MButton from './Compoments/CompomentButton/CompomentButton';
 
 function App() {
 
+  const handleClick = () => {
+    alert('Button clicked!');
+  }
+
 
   return (
     <div className="App">
       Kiểm tra CompomentButton
       <MButton
         variant="contained"
-        size="medium"
-        href="https://mui.com/material-ui/react-button/#sizes"
         color="success"
-        className="mt-10"
+        size="small"
+        className="custome-btn"
+        onClick={handleClick}
       >
         Text
       </MButton>
--- src/Compoments/CompomentButton/CompomentButton.js
+++ src/Compoments/CompomentButton/CompomentButton.js
@@ -6,23 +6,34 @@ import { red } from '@mui/material/colors';
 
 const MButton = (props) => {
 
-    const { children, color, disabled, size, variant, sx, href, className } = props
+    const { children, color, disabled, size, variant, href, className, onClick, leftIcon, rightIcon } = props
 
+    // const buttonClassName = `custom-button ${className || ''}`;
+
+    const buttonClassName = `${className || ''}`;
+
+    // const buttonStyles = {
+    //     color: {color},
+    //     disabled: disabled,
+    //     size: size
+    // }
 
     return (
         <Button
             children={children}
             color={color}
-            disabled={disabled}
-            size={size}
+            disabled = {disabled}
+            size = {size}
             variant={variant}
-            sx={sx}
             href={href}
-            className={`${className}`}
+            className={buttonClassName}
+            onClick={onClick}
+            leftIcon={leftIcon}
+            rightIcon={rightIcon}
+        // buttonStyles={buttonStyles}
         >
         </Button>
     );
 };
-
 
 export default MButton;
